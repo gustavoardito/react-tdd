801732bc61adced3e3998c215fcaa8c2d31836c4
Gustavo Ardito <gardito@eventbrite.com>
2018-09-19 14:07:53 -0300

    add 2nd tests

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -12,3 +12,7 @@ const app = shallow(<App />);
 it('renders correctly', () => {
     expect(app).toMatchSnapshot();
 });
+
+it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
