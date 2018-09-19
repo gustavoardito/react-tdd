6eeb4d98b659a1d3fd8eed37df5d5ac80f7bf9a2
Gustavo Ardito <gardito@eventbrite.com>
2018-09-19 16:10:07 -0300

    adding items to list

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -16,3 +16,14 @@ it('renders correctly', () => {
 it('initializes the `state` with an empty list of gifts', () => {
     expect(app.state().gifts).toEqual([]);
 });
+
+it('adds a new gift to `state`when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{ id: 1 }]);
+});
+
+it('add a new gift to the rendered list when clicking the `add gift`', () => {
+    app.find('.btn-add').simulate('click');
+
+    expect(app.find('.gift-list').children().length).toEqual(2);
+});
