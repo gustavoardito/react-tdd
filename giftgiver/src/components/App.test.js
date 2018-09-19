d27d73cbda407e8b236c6d6cb94d83a5501bc135
Gustavo Ardito <gardito@eventbrite.com>
2018-09-19 14:00:22 -0300

    first test passing

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
+
+// setup file
+configure({ adapter: new Adapter() });
 
 const app = shallow(<App />);
 
