bb5a5a74421d8daec54fd9f5fba6b4b36ead4566
Gustavo Ardito <gardito@eventbrite.com>
2018-09-21 22:59:01 -0300

    apply bdd

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -7,23 +7,33 @@ import App from './App';
 // setup file
 configure({ adapter: new Adapter() });
 
-const app = shallow(<App />);
+describe('App', () => {
+    const app = shallow(<App />);
 
-it('renders correctly', () => {
-    expect(app).toMatchSnapshot();
+    it('renders correctly', () => {
+        expect(app).toMatchSnapshot();
+    });
+
+    it('initializes the `state` with an empty list of gifts', () => {
+        expect(app.state().gifts).toEqual([]);
+    });
+
+    describe('when clicking the `add-gift` button', () => {
+        beforeEach(() => {
+            app.find('.btn-add').simulate('click');
+        });
+
+        afterEach(() => {
+            app.setState({ gifts: []});
+        });
+
+        it('adds a new gift to `state`when clicking the `add gift` button', () => {
+            expect(app.state().gifts).toEqual([{ id: 1 }]);
+        });
+
+        it('add a new gift to the rendered list when clicking the `add gift`', () => {
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        });
+    });
 });
 
-it('initializes the `state` with an empty list of gifts', () => {
-    expect(app.state().gifts).toEqual([]);
-});
-
-it('adds a new gift to `state`when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
-    expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
-
-it('add a new gift to the rendered list when clicking the `add gift`', () => {
-    app.find('.btn-add').simulate('click');
-
-    expect(app.find('.gift-list').children().length).toEqual(2);
-});
