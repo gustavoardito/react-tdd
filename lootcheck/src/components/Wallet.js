ed9644e5e63b18581ff0cc5775e81bc5d2fd66c4
Gustavo Ardito <gardito@eventbrite.com>
2018-09-23 23:37:15 -0300

    add deposit

--- lootcheck/src/components/Wallet.js
+++ lootcheck/src/components/Wallet.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {deposit} from '../actions/balance';
 
 export class Wallet extends Component{
+    constructor() {
+        super();
+
+        this.state = {balance: undefined};
+    }
+
+    updateBalance = event => this.setState({balance: parseInt(event.target.value, 10)});
+
+    deposit = () => this.props.deposit(this.state.balance)
     render() {
         return (
             <div>
                 <h3 className='balance'>
                     Wallet balance: {this.props.balance}
                 </h3>
+                <br />
+                <input className='input-wallet' onChange={this.updateBalance}/>
+                <button className='btn-deposit' onClick={this.deposit}>Deposit</button>
             </div>
         )
     }
 }
 
-export default connect(state => { return {balance: state}}, null)(Wallet);
+export default connect(state => { return {balance: state}}, {deposit})(Wallet);
