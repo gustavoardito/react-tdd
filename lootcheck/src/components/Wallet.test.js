01986462800ec44bea10075228f227b5cef9d125
Gustavo Ardito <gardito@eventbrite.com>
2018-09-23 23:43:25 -0300

    add withdraw btn

--- lootcheck/src/components/Wallet.test.js
+++ lootcheck/src/components/Wallet.test.js
@@ -4,7 +4,8 @@ import {Wallet} from './Wallet';
 
 describe('Wallet', () => {
     const mockDeposit = jest.fn();
-    const props = {balance: 20, deposit: mockDeposit};
+    const mockWithdraw = jest.fn();
+    const props = {balance: 20, deposit: mockDeposit, withdraw: mockWithdraw};
     const wallet = shallow(<Wallet {...props}/>);
 
     it('renders properly', () => {
@@ -39,5 +40,12 @@ describe('Wallet', () => {
             });
         });
 
+        describe('and the user want to make a withdrawal', () => {
+            beforeEach(() => wallet.find('.btn-withdraw').simulate('click'));
+
+            it('dispatches the `withdraw()`it receives from props with local balance', () => {
+                expect(mockWithdraw).toHaveBeenCalledWith(parseInt(userBalance, 10));
+            });
+        });
     });
 });
